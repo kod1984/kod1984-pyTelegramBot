3f4de5117f4f8b6bcb26df0a231094b524ce4776
kod1984 <9452660@gmail.com>
2023-04-20 17:56:21 +0300

    2st commit

--- cb.py
+++ cb.py
@@ -24,15 +24,17 @@ admins           =readFileOrCreateNew('adminsID.txt')         #read list of acti
 adminsNew        =readFileOrCreateNew('adminsNewID.txt')      #read list of admins applicant
 applicantNew     =readFileOrCreateNew('newApplicantID.txt')   #read list of new job applicant
 
+ADMIN_MENU=".\nМожешь добавлять меня в любые группы.\nПо команде /sendall отправлю сообщение во все группы в которые был добавлен ранее.\n /updatenews обновит новость для рассылки. Формат: \n/updatenews Какая-то новость\n/shownews покажет текущую сохраненную новость.\n/showApplicants покажет список желающих поджобать, а /clearApplicants очистит его, будь внимателен."
+WORKER_MENU=".\nХочешь работать у нас? Жми /wantjob и наш менеджер свяжется с тобой!"
 
 #read string with news message
 try:
-    news_file=open('chatNews.txt','r')
+    news_file=open('chatNews.txt','r', encoding="utf-8")
 except IOError:
     news_file=open('chatNews.txt','w', encoding="utf-8")
     news_file.write("Какие-то новости для группы\nОбновить можно через бот и его команду /updatenews.")
     news_file.close()
-    news_file=open('chatNews.txt','r')
+    news_file=open('chatNews.txt','r', encoding="utf-8")
 news=news_file.read()
 news_file.close
 
@@ -89,9 +91,9 @@ def updateNews(newNews):
 async def start(message: types.Message):
     if message.chat.type == 'private':
         if NotElInArr(admins,message.from_user.id) == False:
-            await bot.send_message(message.from_user.id,"Приветы, "+message.from_user.full_name+".\nМожешь добавлять меня в любые группы.\nПо команде /sendall отправлю сообщение во все группы в которые был добавлен ранее.\n /updatenews обновит новость для рассылки. Формат: \n/updatenews Какая-то новость\n/shownews покажет текущую сохраненную новость.\n/showApplicants покажет список желающих поджобать, а /clearApplicants очистит его, будь внимателен.")
+            await bot.send_message(message.from_user.id,"Приветы, "+message.from_user.full_name+ADMIN_MENU)
         else:
-            await bot.send_message(message.from_user.id,"Привет, "+message.from_user.full_name+".\nХочешь работать у нас? Жми /wantjob и наш менеджер свяжется с тобой!")
+            await bot.send_message(message.from_user.id,"Привет, "+message.from_user.full_name+WORKER_MENU)
 
 
 @dp.message_handler(commands=['updatenews'])
@@ -111,6 +113,8 @@ async def updatenews(message: types.Message):
 async def shownews(message: types.Message):
     if (message.chat.type == 'private' and NotElInArr(admins,message.from_user.id) == False):
         await bot.send_message(message.from_user.id,news)
+        await bot.send_message(message.from_user.id,message.from_user.full_name+ADMIN_MENU)
+
 
 
 @dp.message_handler(commands=['clearApplicants'])
@@ -118,6 +122,8 @@ async def clearApplicants(message: types.Message):
     if (message.chat.type == 'private' and NotElInArr(admins,message.from_user.id) == False):
         clearApplicants()
         await bot.send_message(message.from_user.id,"Список пуст.")
+        await bot.send_message(message.from_user.id,message.from_user.full_name+ADMIN_MENU)
+
 
 
 
@@ -128,6 +134,8 @@ async def showApplicants(message: types.Message):
         for applicant in applicantNew:
             rep=rep+"\n"+str(applicant)
         await bot.send_message(message.from_user.id,rep)
+        await bot.send_message(message.from_user.id,message.from_user.full_name+ADMIN_MENU)
+
 
 @dp.message_handler(commands=['viewgroups'])
 async def viewgroups(message: types.Message):
@@ -136,6 +144,8 @@ async def viewgroups(message: types.Message):
         for group in lines:
             rep=rep+"\ntg://group?id="+str(group)
         await bot.send_message(message.from_user.id,rep)
+        await bot.send_message(message.from_user.id,message.from_user.full_name+ADMIN_MENU)
+
 
 
 
@@ -155,6 +165,8 @@ async def start(message: types.Message):
             addApplicationNewID(message.from_user.url)
             addApplicationNewID(message.from_user.mention)
             await bot.send_message(message.from_user.id,"Спасибо за заявку, "+message.from_user.full_name+". Наш менеджер свяжется с вами в ближайшее время.")
+            await bot.send_message(message.from_user.id,message.from_user.full_name+WORKER_MENU)
+
 
 
 
@@ -175,6 +187,7 @@ async def start(message: types.Message):
     if (message.chat.type == 'private' and NotElInArr(admins,message.from_user.id) == False ):
         for lineEl in lines:
             await bot.send_message(lineEl,news)
+            await bot.send_message(message.from_user.id,message.from_user.full_name+ADMIN_MENU)
 
 
 
